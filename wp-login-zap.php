b42ab16f557f90a997c9e5ada0b73c9973e39e34
Sanzeeb Aryal <sanzeeb.aryal@gmail.com>
2019-10-17 09:45:32 +0545

    Add User IP and Browser Information

--- wp-login-zap.php
+++ wp-login-zap.php
@@ -129,8 +129,127 @@ function wplz_login( $user_login, $user ) {
 		esc_html__( 'ID', 'wp-login-zap' ) => $user->ID,
 		esc_html__( 'Username', 'wp-login-zap' ) => $user_login,
 		esc_html__( 'Email', 'wp-login-zap' ) => $user->user_email,
-		apply_filters( 'wp_login_zap_logged_in_time', esc_html__( 'Logged-in Time', 'wp-login-zap' ) ) => current_time( 'mysql' )
+		apply_filters( 'wp_login_zap_logged_in_time', esc_html__( 'Logged-in Time', 'wp-login-zap' ) ) => current_time( 'mysql' ),
+		esc_html__( 'User IP Address', 'wp-login-zap' ) => wplz_get_ip_address(),
+		esc_html__( 'Browser', 'wp-login-zap' ) => wplz_get_browser()
 	));
 
 	wplz_send_data_to_zapier( $data );	
+}
+
+/**
+ * Get User IP Address.
+ *
+ * @see  https://stackoverflow.com/a/2031935/5608921
+ *
+ * @since  1.1.0
+ *
+ * @return string IP Address
+ */
+function wplz_get_ip_address() {
+
+	$ip = '';
+
+    foreach ( array( 'HTTP_CLIENT_IP', 'HTTP_X_FORWARDED_FOR', 'HTTP_X_FORWARDED', 'HTTP_X_CLUSTER_CLIENT_IP', 'HTTP_FORWARDED_FOR', 'HTTP_FORWARDED', 'REMOTE_ADDR' ) as $key ) {
+        if ( array_key_exists( $key, $_SERVER ) === true ) {
+            foreach ( explode( ',', $_SERVER[$key] ) as $ip ) {
+                $ip = trim( $ip ); // just to be safe
+
+                if ( filter_var( $ip, FILTER_VALIDATE_IP, FILTER_FLAG_NO_PRIV_RANGE | FILTER_FLAG_NO_RES_RANGE ) !== false ) {
+                    return $ip;
+                }
+            }
+        }
+    }
+
+    return $ip;
+}
+
+/**
+ * Get User Browser Information.
+ *
+ * @see  https://www.php.net/manual/en/function.get-browser.php#101125
+ *
+ * @since  1.0.1
+ *
+ * @return string
+ */
+function wplz_get_browser() {
+
+    $u_agent 	= $_SERVER['HTTP_USER_AGENT'];
+    $bname 		= 'Unknown';
+    $platform 	= 'Unknown';
+    $Version	= "";
+
+    //First get the platform?
+    if ( preg_match( '/linux/i', $u_agent ) ) {
+        $platform = 'linux';
+    } elseif ( preg_match( '/macintosh|mac os x/i', $u_agent ) ) {
+        $platform = 'mac';
+    } elseif ( preg_match( '/windows|win32/i', $u_agent ) ) {
+        $platform = 'windows';
+    }
+
+    // Next get the name of the useragent yes seperately and for good reason
+    if( preg_match('/MSIE/i',$u_agent) && !preg_match('/Opera/i',$u_agent))
+    {
+        $bname = 'Internet Explorer';
+        $ub = "MSIE";
+    }
+    elseif(preg_match('/Firefox/i',$u_agent))
+    {
+        $bname = 'Mozilla Firefox';
+        $ub = "Firefox";
+    }
+    elseif(preg_match('/Chrome/i',$u_agent))
+    {
+        $bname = 'Google Chrome';
+        $ub = "Chrome";
+    }
+    elseif(preg_match('/Safari/i',$u_agent))
+    {
+        $bname = 'Apple Safari';
+        $ub = "Safari";
+    }
+    elseif(preg_match('/Opera/i',$u_agent))
+    {
+        $bname = 'Opera';
+        $ub = "Opera";
+    }
+    elseif(preg_match('/Netscape/i',$u_agent))
+    {
+        $bname = 'Netscape';
+        $ub = "Netscape";
+    }
+
+    // finally get the correct version number
+    $known = array('Version', $ub, 'other');
+    $pattern = '#(?<browser>' . join('|', $known) .
+    ')[/ ]+(?<version>[0-9.|a-zA-Z.]*)#';
+    if (!preg_match_all($pattern, $u_agent, $matches)) {
+        // we have no matching number just continue
+    }
+
+    // see how many we have
+    $i = count($matches['browser']);
+    if ($i != 1) {
+        //we will have two since we are not using 'other' argument yet
+        //see if version is before or after the name
+        if (strripos($u_agent,"Version") < strripos($u_agent,$ub)){
+            $version= $matches['version'][0];
+        }
+        else {
+            $version= $matches['version'][1];
+        }
+    }
+    else {
+        $version= $matches['version'][0];
+    }
+
+    // check if we have a number
+    if ($version==null || $version=="") {$version="?";}
+
+	$browser= "Browser: " . $bname . " " . $version . " on " .$platform . "";
+
+	return $browser;
 }

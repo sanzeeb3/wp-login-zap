5261d40361e6bdbde8675f6824a29e9eb98563ad
Sanzeeb Aryal <sanzeeb.aryal@gmail.com>
2019-10-19 10:09:10 +0545

    wpcs fixes by phpcbf

--- wp-login-zap.php
+++ wp-login-zap.php
@@ -6,9 +6,14 @@
  * Author: Sanjeev Aryal
  * Author URI: http://www.sanjeebaryal.com.np
  * Text Domain: wp-login-zap
+ *
+ * @package    WP Login Zap
+ * @author     Sanjeev Aryal
+ * @since      1.0.0
+ * @license    GPL-3.0+
  */
 
-defined( 'ABSPATH' ) or die( "No script kiddies please!" );
+defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
 
 add_action( 'admin_menu', 'wplz_register_setting_menu' );
 add_action( 'admin_init', 'wplz_save_settings' );
@@ -25,7 +30,7 @@ function wplz_register_setting_menu() {
 
 /**
  * Settings page for WP Login To Zapier
- * 
+ *
  * @since 1.0.0
  */
 function wplz_settings_page() {
@@ -35,16 +40,16 @@ function wplz_settings_page() {
 		<h2 class="wp-heading-inline"><?php esc_html_e( 'WP Login Zap Settings', 'wp-login-zap' ); ?></h2>
 		<form method="post">
 			<table class="form-table">
-			        <tr valign="top">
-			           	<th scope="row"><?php echo esc_html__( 'Webhook URL', 'wp-login-zap' );?></th>
-			        		<td><input style="width:35%" type="url" name="webhook_url" value ="<?php echo esc_url( $webhook_url ); ?>" class="wp-login-zap-webhook-url" /><br/>
-			        		</td>
-			        </tr>
-			        <?php do_action( 'wp_login_zap_settings' );?>
-		            <?php wp_nonce_field( 'wp_login_zap_settings', 'wp_login_zap_settings_nonce' );?>
+					<tr valign="top">
+						   <th scope="row"><?php echo esc_html__( 'Webhook URL', 'wp-login-zap' ); ?></th>
+							<td><input style="width:35%" type="url" name="webhook_url" value ="<?php echo esc_url( $webhook_url ); ?>" class="wp-login-zap-webhook-url" /><br/>
+							</td>
+					</tr>
+					<?php do_action( 'wp_login_zap_settings' ); ?>
+					<?php wp_nonce_field( 'wp_login_zap_settings', 'wp_login_zap_settings_nonce' ); ?>
 
 			</table>
-			    <?php submit_button(); ?>
+				<?php submit_button(); ?>
 		</form>
 	<?php
 }
@@ -56,30 +61,33 @@ function wplz_settings_page() {
  */
 function wplz_save_settings() {
 
-	if( isset( $_POST['wp_login_zap_settings_nonce'] ) ) {
-		if( ! wp_verify_nonce( $_POST['wp_login_zap_settings_nonce'], 'wp_login_zap_settings' )
+	if ( isset( $_POST['wp_login_zap_settings_nonce'] ) ) {
+		if ( ! wp_verify_nonce( $_POST['wp_login_zap_settings_nonce'], 'wp_login_zap_settings' )
 			) {
 			   print 'Nonce Failed!';
 			   exit;
 		} else {
 			$webhook_url = isset( $_POST['webhook_url'] ) ? esc_url_raw( $_POST['webhook_url'] ) : '';
-			$message     = esc_html__( 'Done!', 'wp-login-zap');
+			$message     = esc_html__( 'Done!', 'wp-login-zap' );
 			$class       = 'notice-success';
 
 			update_option( 'webhook_url', $webhook_url );
 
-			if ( filter_var( $webhook_url, FILTER_VALIDATE_URL ) === FALSE) {
+			if ( filter_var( $webhook_url, FILTER_VALIDATE_URL ) === false ) {
 				$message = esc_html__( 'Not a valid webhook URL.' );
 				$class   = 'error';
 			}
 
-			add_action( 'admin_notices', function() use ( $message, $class ) {
-			    ?>
-				    <div class="notice <?php echo $class;?> is-dismissible">
-				        <p><?php echo $message; ?></p>
-				    </div>
-				<?php
-			});
+			add_action(
+				'admin_notices',
+				function() use ( $message, $class ) {
+					?>
+					<div class="notice <?php echo $class; ?> is-dismissible">
+						<p><?php echo $message; ?></p>
+					</div>
+					<?php
+				}
+			);
 		}
 	}
 }
@@ -99,42 +107,48 @@ function wplz_send_data_to_zapier( array $data ) {
 		return;
 	}
 
-    $headers 	 = array( 'Accept: application/json', 'Content-Type: application/json' );
-	$args 		 = apply_filters( 'wp_login_zap_arguments', array(
-					'method'  => 'POST',
-				    'headers' => $headers,
-  	     	        'body'    => json_encode( $data ),
-				));
+	$headers = array( 'Accept: application/json', 'Content-Type: application/json' );
+	$args    = apply_filters(
+		'wp_login_zap_arguments',
+		array(
+			'method'  => 'POST',
+			'headers' => $headers,
+			'body'    => json_encode( $data ),
+		)
+	);
 
-	$result  = wp_remote_post( esc_url_raw( $webhook_url ), $args );
+	$result = wp_remote_post( esc_url_raw( $webhook_url ), $args );
 
-    if ( is_wp_error( $result ) ) {
-        error_log( print_r( $result->get_error_message(), true ) );
-    }
+	if ( is_wp_error( $result ) ) {
+		error_log( print_r( $result->get_error_message(), true ) );
+	}
 
-    do_action( 'wp_login_zap_data_sent', $result, $webhook_url );
+	do_action( 'wp_login_zap_data_sent', $result, $webhook_url );
 }
 
 /**
  * Action when user logs in.
- * 
- * @param  string $user_login Username
- * 
- * @param  obj $user
+ *
+ * @param  $user_login Username.
+ *
+ * @param  $user User Object.
  *
  * @since  1.0.0
  */
 function wplz_login( $user_login, $user ) {
-	$data = apply_filters( 'wp_login_zap_data_sending', array(
-		esc_html__( 'ID', 'wp-login-zap' ) => $user->ID,
-		esc_html__( 'Username', 'wp-login-zap' ) => $user_login,
-		esc_html__( 'Email', 'wp-login-zap' ) => $user->user_email,
-		apply_filters( 'wp_login_zap_logged_in_time', esc_html__( 'Logged-in Time', 'wp-login-zap' ) ) => current_time( 'mysql' ),
-		esc_html__( 'User IP Address', 'wp-login-zap' ) => wplz_get_ip_address(),
-		esc_html__( 'Browser', 'wp-login-zap' ) => wplz_get_browser()
-	));
+	$data = apply_filters(
+		'wp_login_zap_data_sending',
+		array(
+			esc_html__( 'ID', 'wp-login-zap' )       => $user->ID,
+			esc_html__( 'Username', 'wp-login-zap' ) => $user_login,
+			esc_html__( 'Email', 'wp-login-zap' )    => $user->user_email,
+			apply_filters( 'wp_login_zap_logged_in_time', esc_html__( 'Logged-in Time', 'wp-login-zap' ) ) => current_time( 'mysql' ),
+			esc_html__( 'User IP Address', 'wp-login-zap' ) => wplz_get_ip_address(),
+			esc_html__( 'Browser', 'wp-login-zap' )  => wplz_get_browser(),
+		)
+	);
 
-	wplz_send_data_to_zapier( $data );	
+	wplz_send_data_to_zapier( $data );
 }
 
 /**
@@ -150,19 +164,19 @@ function wplz_get_ip_address() {
 
 	$ip = '';
 
-    foreach ( array( 'HTTP_CLIENT_IP', 'HTTP_X_FORWARDED_FOR', 'HTTP_X_FORWARDED', 'HTTP_X_CLUSTER_CLIENT_IP', 'HTTP_FORWARDED_FOR', 'HTTP_FORWARDED', 'REMOTE_ADDR' ) as $key ) {
-        if ( array_key_exists( $key, $_SERVER ) === true ) {
-            foreach ( explode( ',', $_SERVER[$key] ) as $ip ) {
-                $ip = trim( $ip ); // just to be safe
+	foreach ( array( 'HTTP_CLIENT_IP', 'HTTP_X_FORWARDED_FOR', 'HTTP_X_FORWARDED', 'HTTP_X_CLUSTER_CLIENT_IP', 'HTTP_FORWARDED_FOR', 'HTTP_FORWARDED', 'REMOTE_ADDR' ) as $key ) {
+		if ( array_key_exists( $key, $_SERVER ) === true ) {
+			foreach ( explode( ',', $_SERVER[ $key ] ) as $ip ) {
+				$ip = trim( $ip ); // just to be safe
 
-                if ( filter_var( $ip, FILTER_VALIDATE_IP, FILTER_FLAG_NO_PRIV_RANGE | FILTER_FLAG_NO_RES_RANGE ) !== false ) {
-                    return $ip;
-                }
-            }
-        }
-    }
+				if ( filter_var( $ip, FILTER_VALIDATE_IP, FILTER_FLAG_NO_PRIV_RANGE | FILTER_FLAG_NO_RES_RANGE ) !== false ) {
+					return $ip;
+				}
+			}
+		}
+	}
 
-    return $ip;
+	return $ip;
 }
 
 /**
@@ -176,80 +190,68 @@ function wplz_get_ip_address() {
  */
 function wplz_get_browser() {
 
-    $u_agent 	= $_SERVER['HTTP_USER_AGENT'];
-    $bname 		= 'Unknown';
-    $platform 	= 'Unknown';
-    $Version	= "";
+	$u_agent  = $_SERVER['HTTP_USER_AGENT'];
+	$bname    = 'Unknown';
+	$platform = 'Unknown';
+	$Version  = '';
 
-    //First get the platform?
-    if ( preg_match( '/linux/i', $u_agent ) ) {
-        $platform = 'linux';
-    } elseif ( preg_match( '/macintosh|mac os x/i', $u_agent ) ) {
-        $platform = 'mac';
-    } elseif ( preg_match( '/windows|win32/i', $u_agent ) ) {
-        $platform = 'windows';
-    }
+	// First get the platform?
+	if ( preg_match( '/linux/i', $u_agent ) ) {
+		$platform = 'linux';
+	} elseif ( preg_match( '/macintosh|mac os x/i', $u_agent ) ) {
+		$platform = 'mac';
+	} elseif ( preg_match( '/windows|win32/i', $u_agent ) ) {
+		$platform = 'windows';
+	}
 
-    // Next get the name of the useragent yes seperately and for good reason
-    if( preg_match('/MSIE/i',$u_agent) && !preg_match('/Opera/i',$u_agent))
-    {
-        $bname = 'Internet Explorer';
-        $ub = "MSIE";
-    }
-    elseif(preg_match('/Firefox/i',$u_agent))
-    {
-        $bname = 'Mozilla Firefox';
-        $ub = "Firefox";
-    }
-    elseif(preg_match('/Chrome/i',$u_agent))
-    {
-        $bname = 'Google Chrome';
-        $ub = "Chrome";
-    }
-    elseif(preg_match('/Safari/i',$u_agent))
-    {
-        $bname = 'Apple Safari';
-        $ub = "Safari";
-    }
-    elseif(preg_match('/Opera/i',$u_agent))
-    {
-        $bname = 'Opera';
-        $ub = "Opera";
-    }
-    elseif(preg_match('/Netscape/i',$u_agent))
-    {
-        $bname = 'Netscape';
-        $ub = "Netscape";
-    }
+	// Next get the name of the useragent yes seperately and for good reason
+	if ( preg_match( '/MSIE/i', $u_agent ) && ! preg_match( '/Opera/i', $u_agent ) ) {
+		$bname = 'Internet Explorer';
+		$ub    = 'MSIE';
+	} elseif ( preg_match( '/Firefox/i', $u_agent ) ) {
+		$bname = 'Mozilla Firefox';
+		$ub    = 'Firefox';
+	} elseif ( preg_match( '/Chrome/i', $u_agent ) ) {
+		$bname = 'Google Chrome';
+		$ub    = 'Chrome';
+	} elseif ( preg_match( '/Safari/i', $u_agent ) ) {
+		$bname = 'Apple Safari';
+		$ub    = 'Safari';
+	} elseif ( preg_match( '/Opera/i', $u_agent ) ) {
+		$bname = 'Opera';
+		$ub    = 'Opera';
+	} elseif ( preg_match( '/Netscape/i', $u_agent ) ) {
+		$bname = 'Netscape';
+		$ub    = 'Netscape';
+	}
 
-    // finally get the correct version number
-    $known = array('Version', $ub, 'other');
-    $pattern = '#(?<browser>' . join('|', $known) .
-    ')[/ ]+(?<version>[0-9.|a-zA-Z.]*)#';
-    if (!preg_match_all($pattern, $u_agent, $matches)) {
-        // we have no matching number just continue
-    }
+	// finally get the correct version number
+	$known   = array( 'Version', $ub, 'other' );
+	$pattern = '#(?<browser>' . join( '|', $known ) .
+	')[/ ]+(?<version>[0-9.|a-zA-Z.]*)#';
+	if ( ! preg_match_all( $pattern, $u_agent, $matches ) ) {
+		// we have no matching number just continue
+	}
 
-    // see how many we have
-    $i = count($matches['browser']);
-    if ($i != 1) {
-        //we will have two since we are not using 'other' argument yet
-        //see if version is before or after the name
-        if (strripos($u_agent,"Version") < strripos($u_agent,$ub)){
-            $version= $matches['version'][0];
-        }
-        else {
-            $version= $matches['version'][1];
-        }
-    }
-    else {
-        $version= $matches['version'][0];
-    }
+	// see how many we have
+	$i = count( $matches['browser'] );
+	if ( $i != 1 ) {
+		// we will have two since we are not using 'other' argument yet
+		// see if version is before or after the name
+		if ( strripos( $u_agent, 'Version' ) < strripos( $u_agent, $ub ) ) {
+			$version = $matches['version'][0];
+		} else {
+			$version = $matches['version'][1];
+		}
+	} else {
+		$version = $matches['version'][0];
+	}
 
-    // check if we have a number
-    if ($version==null || $version=="") {$version="?";}
+	// check if we have a number
+	if ( $version == null || $version == '' ) {
+		$version = '?';}
 
-	$browser= "Browser: " . $bname . " " . $version . " on " .$platform . "";
+	$browser = 'Browser: ' . $bname . ' ' . $version . ' on ' . $platform . '';
 
 	return $browser;
 }
